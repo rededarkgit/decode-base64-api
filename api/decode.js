bb4938e598129da73da491cba39a3cd5f327ac0d
rededarkgit <rededarkcontato@gmail.com>
2025-05-07 00:46:48 -0300

    Update decode.js

--- api/decode.js
+++ api/decode.js
@@ -1,17 +1,22 @@
+export const config = {
+  api: {
+    bodyParser: false, // importante para aceitar binário
+  },
+};
+
 export default async function handler(req, res) {
-  const { url } = req.query;
-
-  if (!url || !url.startsWith('data:image')) {
-    return res.status(400).json({ error: 'Missing or invalid base64 image string' });
-  }
-
   try {
-    const base64Data = url.split(';base64,').pop();
-    const binary = Buffer.from(base64Data, 'base64');
+    const chunks = [];
+
+    for await (const chunk of req) {
+      chunks.push(chunk);
+    }
+
+    const buffer = Buffer.concat(chunks);
 
     res.setHeader('Content-Type', 'image/png');
     res.setHeader('Content-Disposition', 'inline; filename="image.png"');
-    res.status(200).send(binary);
+    res.status(200).send(buffer);
   } catch (error) {
     res.status(500).json({ error: 'Failed to decode image', details: error.message });
   }
